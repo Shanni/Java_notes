fb6e59ee4288bd39d007fbf553591ab41030e00f
Shanni <shan.liu03@gmail.com>
2016-02-08 16:09:20 -0500

    add new file

---
+++ Step1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package step1;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
+/**
+ *
+ * @author shanliu
+ */
+public class Step1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ArrayList a=new ArrayList();
+        a.add(3);
+        a.add("hi");
+        a.add(new String("hey"));
+        System.out.println(a);
+        System.out.println(a.size());
+        
+        LinkedList l=new LinkedList();
+        System.out.println(l.size());
+        
+    }
+    
+}
