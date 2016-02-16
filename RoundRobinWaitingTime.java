5f0b82cb0babd69e95621deccdb2472513081476
Shan Liu <shan.liu03@gmail.com>
2016-02-16 16:54:31 -0500

    Update RoundRobinWaitingTime.java

--- RoundRobinWaitingTime.java
+++ RoundRobinWaitingTime.java
@@ -2,6 +2,7 @@ import java.util.*;
 class RoundRobin{
     int quant=3;
     public static void main(String[] args){
+        //small test example
         int[] arrive={0,1,4};
         int[] execute={5,2,3};
         RoundRobin a=new RoundRobin();
@@ -12,27 +13,25 @@ class RoundRobin{
             return 0;
         }
         Queue<Integer> q=new LinkedList<Integer>();
-        int exeTime=0;
         int wait=arrive[0];
         int cumTime=0;
         q.offer(0);
-        int i=0;
-        int j=1;
+        int i=0; //current processor
+        int j=1; //next probing processor
         while(!q.isEmpty()){
             i=q.poll();
-            wait+=cumTime-arrive[i];
+            wait+=cumTime-arrive[i]; //how long the processor has been waiting
             //System.out.println("the thing has been waiting "+String.valueOf(cumTime-arrive[i]));
             int runTime=Math.min(execute[i],quant);
             cumTime+=runTime;
             execute[i]-=runTime;
-
             while( j<arrive.length && arrive[j]<=cumTime ){
-                q.offer(j);
+                q.offer(j); //new arriving processor put in queue
                 j+=1;
             }
             if (0<execute[i]){
                 q.offer(i);
-                arrive[i]=cumTime;
+                arrive[i]=cumTime; //update arriving time
             }
           
         }
